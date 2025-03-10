33cd0e1129901c08d284fd19e2fb87f47bbd52bd
CorentinBT <corentinbt0@gmail.com>
2025-03-11 00:00:34 +0100

    Additional keymaps + option adjustments

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,17 +1,24 @@
--- Set leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 -- Disable spacebar behavior in Normal and Visual modes
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 local opts = { noremap = true, silent = true }
 
 -- Navigation
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>", opts)
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>", opts)
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>", opts)
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>", opts)
+
+-- Buffer navigation
+vim.keymap.set("n", "<Tab>", ":bnext <CR>", opts)
+vim.keymap.set("n", "<S-Tab>", ":bprevious <CR>", opts)
+
+-- Adjust split sizes
+vim.keymap.set("n", "<C-Left>", ":vertical resize +3<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize -3<CR>", opts)
+
+-- Netrw
+vim.keymap.set("n", "<leader>e", ":25Lex<CR>", opts)
 
 -- Remap esc
 vim.keymap.set('i', 'jk', '<Esc>', opts)
@@ -23,7 +30,7 @@ vim.keymap.set('n', '<C-s>', '<Cmd> w <CR>', opts)
 vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
 vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
 
--- Keep last yanked when pasting 
+-- Keep last yanked when pasting
 vim.keymap.set('v', 'p', '"_dP', opts)
 
 -- Clear highlights on search when pressing <Esc> in normal mode
@@ -31,3 +38,4 @@ vim.keymap.set('n', '<Esc>', '<Cmd>nohlsearch<CR>', opts)
 
 -- LSP
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set('n', '<C-k><C-d>', vim.lsp.buf.format, opts)
--- lua/options.lua
+++ lua/options.lua
@@ -1,4 +1,11 @@
-vim.opt.nu = true
+vim.g.mapleader = " "
+vim.g.netrw_banner = 0
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
+vim.g.netrw_liststyle = 3
+
+vim.opt.compatible = false
+vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.autoindent = true
@@ -11,8 +18,5 @@ vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.inccommand = 'split'
-
--- Netrw
-vim.g.netrw_banner = 0
-vim.g.netrw_altv = 1
-vim.g.netrw_liststyle = 3
+vim.opt.splitbelow = true
+vim.opt.splitright = true
